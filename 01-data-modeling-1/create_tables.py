d003f5d47e7653e9aaf18a16d57c51795834a09e
Bootsagorn <157880018+Bootsagorn@users.noreply.github.com>
2024-02-01 11:00:15 +0700

    Update create_tables.py
    update table add table repo

--- 01-data-modeling-1/create_tables.py
+++ 01-data-modeling-1/create_tables.py
@@ -8,6 +8,7 @@ PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
 
 table_drop_events = "DROP TABLE IF EXISTS events"
 table_drop_actors = "DROP TABLE IF EXISTS actors"
+table_drop_repo = "DROP TABLE IF EXISTS repo"
 
 table_create_actors = """
     CREATE TABLE IF NOT EXISTS actors (
@@ -16,23 +17,36 @@ table_create_actors = """
         PRIMARY KEY(id)
     )
 """
+table_create_repo = """
+    CREATE TABLE IF NOT EXISTS repo (
+        id int,
+        name text,
+		url text,
+        PRIMARY KEY(id)
+    )
+"""
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events (
         id text,
         type text,
         actor_id int,
+		repo_id int,
         PRIMARY KEY(id),
         CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+		CONSTRAINT fk_repo FOREIGN KEY(repo_id) REFERENCES repo(id)
     )
 """
 
 create_table_queries = [
     table_create_actors,
+    table_create_repo,
     table_create_events,
+
 ]
 drop_table_queries = [
     table_drop_events,
-    table_drop_actors,
+	table_drop_repo,
+	table_drop_actors,
 ]
 
 
