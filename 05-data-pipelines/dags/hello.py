b586ea805587b8a5b7ec8827614dca3b19be8e06
Bootsagorn Dechaphong <157880018+Bootsagorn@users.noreply.github.com>
2024-03-23 11:00:05 +0000

    edit file

--- 05-data-pipelines/dags/hello.py
+++ 05-data-pipelines/dags/hello.py
@@ -1,32 +1,21 @@
 from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.utils import timezone
-from airflow.operators.python import PythonOperator
-
-
-def _say_hello():
-    return "Hello!"
 
 
 with DAG(
     "hello",
-    start_date=timezone.datetime(2022, 11, 1),
-    schedule="@daily",
-    tags=["workshop"],
+    start_date=timezone.datetime(2024, 3 ,23),
+    schedule=None,
+    tags=["DS525"],
 ):
+    start = EmptyOperator(task_id="start")
 
-    say_hello = PythonOperator(
-        task_id="say_hello",
-        python_callable=_say_hello,
+    echo_hello = BashOperator(
+        task = "echo_hello",
+        bash_commands"echo 'hello'",
     )
+    end = EmptyOperator(task_id="end")
 
-    say_hello_2 = PythonOperator(
-        task_id="say_hello_2",
-        python_callable=_say_hello,
-    )
-
-    say_hello_3 = PythonOperator(
-        task_id="say_hello_3",
-        python_callable=_say_hello,
-    )
-
-    say_hello >> say_hello_2 >> say_hello_3
+    start >> end
